7e9196416a86b08ec34fe93dc2cdda0b866de20e
Bob Wilkinson <bob@fourtheye.org>
2023-12-19 19:31:09 +0000

    first commit

---
+++ bin/apt.sh
@@ -0,0 +1,13 @@
+sudo apt install -y software-properties-common apt-transport-https ca-certificates gnupg software-properties-common wget vim git w3m
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/cgoogle.gpg
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl kubernetes-helm containerd docker.io
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo usermod -aG docker $USER && newgrp docker
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_arm64.deb
+sudo dpkg -i minikube_latest_arm64.deb
+
+minikube start
---
+++ bin/mongodb.sh
@@ -0,0 +1,32 @@
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm search repo bitnami
+helm install mongodb bitnami/mongodb  --set image.repository=arm64v8/mongo --set image.tag=latest --set persistence.mountPath=/data/db
+helm ls
+kubectl get all
+kubectl describe deployment.apps/mongodb
+
+MONGO_POD=$(kubectl get pods -o wide | grep ^mongodb | awk '{print $1}')
+kubectl describe pod/${MONGO_POD}
+kubectl logs pod/${MONGO_POD}
+
+kubectl expose deployment mongodb --type=NodePort --port=27017
+kubectl port-forward svc/mongodb 27017:27017 &
+kubectl get all
+
+sudo apt install -y mongodb-clients
+
+> show dbs
+admin   0.000GB
+config  0.000GB
+local   0.000GB
+> use test
+switched to db test
+> db.test.insertOne( {name : "deb" })
+{
+	"acknowledged" : true,
+	"insertedId" : ObjectId("6581e6b0b0501354fed2d100")
+}
+> db.test.find()
+{ "_id" : ObjectId("6581e6b0b0501354fed2d100"), "name" : "deb" }
+> 
+
