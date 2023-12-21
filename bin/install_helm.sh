bfc52a54c3359612a647dde648e71dba30fb2a9e
Bob Wilkinson <bob@fourtheye.org>
2023-12-21 16:01:42 +0000

    add script to add mongodb as helm package

---
+++ bin/install_helm.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+set -xu
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm search repo bitnami
+helm install mongodb bitnami/mongodb  --set image.repository=arm64v8/mongo --set image.tag=latest --set persistence.mountPath=/data/db
+helm ls
