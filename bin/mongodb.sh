4421249ffc54fbb43ccffe3b46fce5b3cc5009a0
Bob Wilkinson <bob@fourtheye.org>
2023-12-21 13:05:56 +0000

    explicitly use minikube kubectl

--- bin/mongodb.sh
+++ bin/mongodb.sh
@@ -1,18 +1,22 @@
+#/bin/bash
+set -x
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm search repo bitnami
 helm install mongodb bitnami/mongodb  --set image.repository=arm64v8/mongo --set image.tag=latest --set persistence.mountPath=/data/db
 helm ls
+
 kubectl="minikube kubectl --"
-kubectl get all
-kubectl describe deployment.apps/mongodb
+minikube kubectl get all
+minikube kubectl describe deployment.apps/mongodb
 
-MONGO_POD=$(kubectl get pods | grep ^mongodb | awk '{print $1}')
-kubectl describe pod/${MONGO_POD}
-kubectl logs pod/${MONGO_POD}
+MONGO_POD=$(minikube kubectl get pods | grep ^mongodb | awk '{print $1}')
+echo MONGO POD IS ${MONGO_POD}
+minikube kubectl describe pod/${MONGO_POD}
+minikube kubectl logs pod/${MONGO_POD}
 
-kubectl expose deployment mongodb --type=NodePort --port=27017
-kubectl port-forward svc/mongodb 27017:27017 &
-kubectl get all
+minikube kubectl expose deployment mongodb --type=NodePort --port=27017
+minikube kubectl port-forward svc/mongodb 27017:27017 &
+minikube kubectl get all
 
 sudo apt install -y mongodb-clients
 
@@ -30,4 +34,4 @@ sudo apt install -y mongodb-clients
 # > db.test.find()
 # { "_id" : ObjectId("6581e6b0b0501354fed2d100"), "name" : "deb" }
 # > 
-# 
+#
