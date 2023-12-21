ebf63ce35eabd7fdad07ecae54781b9b709927da
Bob Wilkinson <bob@fourtheye.org>
2023-12-21 18:25:04 +0000

    avoid chaining commands

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,8 @@
 set -xu
 sudo apt update
 sudo apt install -y software-properties-common apt-transport-https ca-certificates gnupg software-properties-common wget vim git w3m kubernetes-helm containerd docker.io
-sudo usermod -aG docker $USER && newgrp docker
+sudo usermod -aG docker $USER 
+sudo newgrp docker
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_arm64.deb
 sudo dpkg -i minikube_latest_arm64.deb
