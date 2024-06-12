dfad737abe08e67e20cc80175ee859192c047a9b
Robert Wilkinson <bob@fourtheye.org>
2024-06-12 08:17:35 +0000

    remove search

--- bin/install_mongodb_helm.sh
+++ bin/install_mongodb_helm.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 set -xu
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm search repo bitnami
 helm install mongodb bitnami/mongodb  --set image.repository=arm64v8/mongo --set image.tag=latest --set persistence.mountPath=/data/db
 helm ls
