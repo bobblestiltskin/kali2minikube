ee79830e81b784bd40e1a83f084d38a51faf4174
Robert Wilkinson <bob@fourtheye.org>
2024-06-12 08:13:59 +0000

    a tidier minikube install script

--- bin/apt.sh
+++ bin/apt.sh
@@ -1,11 +1,5 @@
-sudo apt install -y software-properties-common apt-transport-https ca-certificates gnupg software-properties-common wget vim git w3m
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/cgoogle.gpg
-# sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-
 sudo apt update
-sudo apt install -y kubernetes-helm containerd docker.io
-# sudo apt install -y kubelet kubeadm kubectl kubernetes-helm containerd docker.io
-# sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt install -y software-properties-common apt-transport-https ca-certificates gnupg software-properties-common wget vim git w3m kubernetes-helm containerd docker.io
 sudo usermod -aG docker $USER && newgrp docker
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_arm64.deb
